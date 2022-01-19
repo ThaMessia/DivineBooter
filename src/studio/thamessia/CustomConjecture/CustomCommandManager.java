874815f7e32398a416fe91e7dde65457bfdadffd
ThaMessia <simple35@virgilio.it>
2022-01-19 23:42:43 +0100

    Fixed customizable command addon. Ready for version 0.2-BETA

--- src/studio/thamessia/CustomConjecture/CustomCommandManager.java
+++ src/studio/thamessia/CustomConjecture/CustomCommandManager.java
@@ -2,13 +2,39 @@ package studio.thamessia.CustomConjecture;
 
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.InputStreamReader;
 
 public class CustomCommandManager {
     public static void manager() {
         JavaCompiler javaCompiler = ToolProvider.getSystemJavaCompiler();
-        javaCompiler.run(null, null, null, "divineCustomCommand.txt");
+        javaCompiler.run(null, null, null, "CustomCommandExecutor.java");
 
         CustomCommandExecutor customCommandExecutor = new CustomCommandExecutor();
         customCommandExecutor.execute();
+
+        try {
+            Process runtime = Runtime.getRuntime().exec("java CustomCommandExecutor");
+
+            BufferedReader bufferedReaderManagerONE = new BufferedReader(new InputStreamReader(runtime.getInputStream()));
+            BufferedReader bufferedReaderManagerTWO = new BufferedReader(new InputStreamReader(runtime.getErrorStream()));
+
+            String str = "sesso";
+            String ing = "pazzo";
+
+
+            while ((str = bufferedReaderManagerONE.readLine()) != null) {
+                System.out.println(str);
+            }
+
+            while ((ing = bufferedReaderManagerTWO.readLine()) != null) {
+                System.out.println(ing);
+            }
+
+            //System.out.println("hello worldt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
